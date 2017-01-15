955a20a5a23fa545419e577d202a5fac6c2166dd
domingo <domingo.a.avila@gmail.com>
2017-01-15 16:37:48 -0300

    update async

---
+++ Async/2Samples/WebAppAsync/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppAsync.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            using (var client = new HttpClient())
+            {
+                var httpMessage = await client.GetAsync("http://www.filipekberg.se/rss/").ConfigureAwait(false);
+
+                var content = await httpMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                return Content(content);
+            }
+
+            //return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
